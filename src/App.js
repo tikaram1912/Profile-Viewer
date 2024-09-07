8c0c854eff157502355f2983ef13a8a77e22eb57
Tikaram <tikarammahato377@gmail.com>
2024-09-08 01:02:51 +0530

    initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import reviews from "./data";
+import Testimonials from "./components/Testimonials";
+
+const App = () => {
+  return (
+    <div >
+      App
+    </div>
+  );
+};
+
+export default App;
